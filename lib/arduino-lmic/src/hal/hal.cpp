87b7ef71a4edd06338cca7c0f57526a23a8adff2
Nicolas Graziano <nicolas.graziano@engie.com>
2018-09-22 23:27:23 +0200

    Remove old hack.

--- lib/arduino-lmic/src/hal/hal.cpp
+++ lib/arduino-lmic/src/hal/hal.cpp
@@ -194,13 +194,6 @@ bool hal_checkTimer(OsTime const &time) {
   auto delta = time - hal_ticks();
   if (delta <= OsDeltaTime(0))
     return true;
-
-  // HACK for a bug (I will track it down.)
-  if (delta >= OsDeltaTime::from_sec(60 * 60)) {
-    PRINT_DEBUG_2("WARN delta is too big, execute now ref : %lu, delta : %li",
-                  time, delta.tick());
-    return true;
-  }
   return false;
 }
 
