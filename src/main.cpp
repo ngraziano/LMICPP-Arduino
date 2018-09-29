9154d6d228287f446aa2b01ff4551226976bda5d
Nicolas Graziano <nicolas.graziano@engie.com>
2018-09-29 12:18:08 +0200

    First version with APDS9960.

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 #include <hal/hal.h>
 #include <SPI.h>
 #include <LowPower.h>
+#include <SparkFun_APDS9960.h>
 
 #define DEVICE_POSTDETECT
 #include "lorakeys.h"
 
 void do_send();
+void reset_and_do_send();
 
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
@@ -29,15 +31,19 @@ void getDevEui(uint8_t *buf) { memcpy_P(buf, DEVEUI, 8); }
 // The key shown here is the semtech default key.
 // defined in lorakeys.h
 
-uint8_t data[4] = {};
+uint8_t data[12] = {};
 
 OsJob sendjob;
 
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
 
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-OsDeltaTime TX_INTERVAL = OsDeltaTime::from_sec(60 * 5);
+OsDeltaTime TX_INTERVAL = OsDeltaTime::from_sec(60 * 60);
+
+// keep ON for one minute
+OsDeltaTime TX_ONLENGTH = OsDeltaTime::from_sec(1 * 60);
 
 const unsigned int BAUDRATE = 19200;
 
@@ -48,6 +54,18 @@ const lmic_pinmap lmic_pins = {
     .rst = 14,
     .dio = {9, 8},
 };
+
+bool apds_flag = false;
+bool apds_new = false;
+
+void apdsInterrupt() {
+    if(apds_flag)
+        return;
+    if(!digitalRead(3)) {
+        apds_flag = true;
+        apds_new = true;
+    }
+}
 
 void onEvent(ev_t ev)
 {
@@ -73,8 +91,16 @@ void onEvent(ev_t ev)
         {
             PRINT_DEBUG_2("Received %d  bytes of payload", LMIC.dataLen);
         }
-        // Schedule next transmission
-        sendjob.setTimedCallback(os_getTime() + TX_INTERVAL, do_send);
+        // we have transmit
+        if(apds_flag) {
+
+            // schedule back to off
+            sendjob.setTimedCallback(os_getTime() + TX_ONLENGTH, reset_and_do_send);
+        } else {
+            // Schedule next transmission
+            sendjob.setTimedCallback(os_getTime() + TX_INTERVAL, do_send);
+        }
+        
         break;
     case EV_RESET:
         PRINT_DEBUG_2("EV_RESET");
@@ -89,6 +115,12 @@ void onEvent(ev_t ev)
         PRINT_DEBUG_2("Unknown event");
         break;
     }
+}
+
+void reset_and_do_send() {
+    apds.clearProximityInt();
+    apds_flag = false;
+    do_send();
 }
 
 void do_send()
@@ -112,10 +144,24 @@ void do_send()
         uint16_t val = analogRead(A1) * (6.6/1024*100 );
         data[2] = val >> 8;
         data[3] = val;
+        
+        data[4] = 2;
+        data[5] = 2;
+        uint8_t prox;
+        apds.readProximity(prox);
+        Serial.print("SEND dist : ");
+        Serial.println(prox, DEC);
+        val = prox * 10;
+        data[6] = val >> 8;
+        data[7] = val;
+
+        data[8] = 3;
+        data[9] = 0;
+        data[10] = apds_flag ? 1:0;
 
 
         // Prepare upstream data transmission at the next possible time.
-        LMIC.setTxData2(1, (uint8_t *)data, 4, false);
+        LMIC.setTxData2(1, (uint8_t *)data,11 , false);
         PRINT_DEBUG_1("Packet queued");
     }
     // Next TX is scheduled after TX_COMPLETE event.
@@ -141,12 +187,34 @@ void pciSetup(byte pin)
     PCICR |= bit(digitalPinToPCICRbit(pin));                   // enable interrupt for the group
 }
 
+
+void initProximitySensor() {
+    if ( apds.init() ) {
+        Serial.println(F("APDS-9960 initialization complete"));
+    } else {
+        Serial.println(F("Something went wrong during APDS-9960 init!"));
+    }
+
+    // Set proximity interrupt thresholds
+    apds.setProximityIntLowThreshold(0);
+    apds.setProximityIntHighThreshold(80);
+    
+    apds.enableProximitySensor(true);
+}
+
+void disableProximitySensor() {
+    apds.disablePower();
+}
+
 void setup()
 {
 #if LMIC_DEBUG_LEVEL > 0
     Serial.begin(BAUDRATE);
     Serial.println(F("Starting"));
 #endif
+    pinMode(3, INPUT);
+    attachInterrupt(digitalPinToInterrupt(3), apdsInterrupt, FALLING);
+    initProximitySensor();
 
     pciSetup(lmic_pins.dio[0]);
     pciSetup(lmic_pins.dio[1]);
@@ -165,15 +233,7 @@ void setup()
     LMIC.setArtEuiCallback(getArtEui);
 
     // set clock error to allow good connection.
-    LMIC.setClockError(MAX_CLOCK_ERROR * 5 / 100);
-
-    // for(int i = 1; i <= 8; i++) LMIC_disableChannel(i);
-    // LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
-
-    // TTN uses SF9 for its RX2 window.
-    // LMIC.dn2Dr = DR_SF9;
-    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    // LMIC_setDrTxpow(DR_SF9,14);
+    LMIC.setClockError(MAX_CLOCK_ERROR * 3 / 100);
 
     // Start job (sending automatically starts OTAA too)
     do_send();
@@ -228,11 +288,14 @@ void powersave(OsDeltaTime const &maxTime)
     Serial.flush();
 #endif
 
-    for (uint16_t nbsleep = maxTime / duration_selected; nbsleep > 0; nbsleep--)
+    for (uint16_t nbsleep = maxTime / duration_selected; nbsleep > 0 && !apds_new; nbsleep--)
     {
 
         LowPower.powerDown(period_selected, ADC_OFF, BOD_OFF);
         hal_add_time_in_sleep(duration_selected);
+
+        // Check if we are wakeup by external pin. 
+        apdsInterrupt();
     }
 
 #if LMIC_DEBUG_LEVEL > 0
@@ -247,5 +310,10 @@ void loop()
     if (to_wait > 0 && hal_is_sleep_allow())
     {
         powersave(to_wait);
+    }
+    if(apds_new) {
+        Serial.println("DETECT !!!");
+        apds_new = false;
+        do_send();
     }
 }
