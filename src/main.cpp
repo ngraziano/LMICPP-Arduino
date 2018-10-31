29594348c0a5d1c4f13c0d4573cb6a9f002857f6
Nicolas Graziano <nicolas.graziano@engie.com>
2018-10-31 19:44:27 +0100

    Main for a simple balise.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,11 @@
 #include <Arduino.h>
 
 #include <lmic.h>
-
 #include <hal/hal_io.h>
 
 #include <LowPower.h>
-#include <SparkFun_APDS9960.h>
 
-#define DEVICE_POSTDETECT
+#define DEVICE_BALISE
 #include "lorakeys.h"
 
 void do_send();
@@ -32,17 +30,14 @@ void getDevEui(uint8_t *buf) { memcpy_P(buf, DEVEUI, 8); }
 // The key shown here is the semtech default key.
 // defined in lorakeys.h
 
-uint8_t data[12] = {};
+uint8_t data[2] = {};
 
 OsJob sendjob;
 
-SparkFun_APDS9960 apds = SparkFun_APDS9960();
-
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const OsDeltaTime TX_INTERVAL = OsDeltaTime::from_sec(60 * 60);
-// keep ON for one minute
-const OsDeltaTime TX_ONLENGTH = OsDeltaTime::from_sec(20);
+const OsDeltaTime TX_INTERVAL = OsDeltaTime::from_sec(2 * 60);
+
 
 const unsigned int BAUDRATE = 19200;
 
@@ -55,51 +50,6 @@ const lmic_pinmap lmic_pins = {
     .dio = {9, 8},
 };
 LmicEu868 LMIC(lmic_pins);
-
-
-const uint8_t NUMBERTIME_TO_SEND = 3;
-uint8_t apds_tosend = 0;
-bool apds_new = false;
-
-void initProximitySensor(uint8_t threshold)
-{
-    if (apds.init())
-    {
-        Serial.println(F("APDS-9960 initialization complete"));
-    }
-    else
-    {
-        Serial.println(F("Something went wrong during APDS-9960 init!"));
-    }
-
-    // Set proximity interrupt thresholds
-    apds.setProximityIntLowThreshold(0);
-    apds.setProximityIntHighThreshold(threshold);
-    apds.setProximityGain(DEFAULT_PGAIN);
-    apds.setLEDDrive(LED_DRIVE_50MA);
-    apds.setProximityIntEnable(1);
-    apds.enablePower();
-    apds.setMode(PROXIMITY, 1);
-}
-
-void disableProximitySensor()
-{
-    apds.setMode(PROXIMITY, 0);
-    apds.disablePower();
-}
-
-void apdsInterrupt()
-{
-    // If we are still sending detect event
-    // do not try do detect another one.
-    if (apds_tosend > 0)
-        return;
-    if (!digitalRead(3))
-    {
-        apds_tosend = NUMBERTIME_TO_SEND;
-        apds_new = true;
-    }
-}
 
 void onEvent(EventType ev)
 {
@@ -129,24 +79,12 @@ void onEvent(EventType ev)
                 uint8_t port = LMIC.frame[LMIC.dataBeg - 1];
                 if (port == 9)
                 {
-                    disableProximitySensor();
-                    delay(500);
-                    initProximitySensor(LMIC.frame[LMIC.dataBeg]);
                 }
             }
         }
         // we have transmit
-        if (apds_tosend)
-        {
-
-            // schedule back to off
-            sendjob.setTimedCallback(os_getTime() + TX_ONLENGTH, reset_and_do_send);
-        }
-        else
-        {
             // Schedule next transmission
             sendjob.setTimedCallback(os_getTime() + TX_INTERVAL, do_send);
-        }
 
         break;
     case EventType::RESET:
@@ -164,15 +102,7 @@ void onEvent(EventType ev)
     }
 }
 
-void reset_and_do_send()
-{
-    // we have sent one
-    if (apds_tosend > 0)
-        apds_tosend--;
-    if (apds_tosend == 0)
-        apds.clearProximityInt();
-    do_send();
-}
+
 
 void do_send()
 {
@@ -185,10 +115,7 @@ void do_send()
     }
     else
     {
-        const uint8_t pinCmd = 6;
-        pinMode(pinCmd, OUTPUT);
-        digitalWrite(pinCmd, 1);
-        delay(100);
+
         // battery
         data[0] = 1;
         data[1] = 2;
@@ -196,20 +123,8 @@ void do_send()
         data[2] = val >> 8;
         data[3] = val;
 
-        data[4] = 2;
-        data[5] = 2;
-        uint8_t prox;
-        apds.readProximity(prox);
-        val = prox * 100;
-        data[6] = val >> 8;
-        data[7] = val;
-
-        data[8] = 3;
-        data[9] = 0;
-        data[10] = apds_tosend > 0 ? 1 : 0;
-
         // Prepare upstream data transmission at the next possible time.
-        LMIC.setTxData2(1, (uint8_t *)data, 11, false);
+        LMIC.setTxData2(1, data, 4, false);
         PRINT_DEBUG_1("Packet queued");
     }
     // Next TX is scheduled after TX_COMPLETE event.
@@ -236,16 +151,27 @@ void pciSetup(byte pin)
     PCICR |= bit(digitalPinToPCICRbit(pin));                   // enable interrupt for the group
 }
 
+void powersave(OsDeltaTime const &maxTime);
+
+void testDuration(int32_t ms)
+{
+    const auto delta = OsDeltaTime::from_ms(ms);
+    PRINT_DEBUG_1("Test sleep time for %i ms.",ms);
+    const OsTime start = os_getTime();
+    PRINT_DEBUG_1("Start Test sleep time.");
+    powersave(delta);
+    const OsTime end = os_getTime();
+    PRINT_DEBUG_1("End Test sleep time.");
+    PRINT_DEBUG_1("Test Time should be : %d ms", (end-start).to_ms());
+}
+
+
 void setup()
 {
 #if LMIC_DEBUG_LEVEL > 0
     Serial.begin(BAUDRATE);
     Serial.println(F("Starting"));
 #endif
-    pinMode(3, INPUT);
-    attachInterrupt(digitalPinToInterrupt(3), apdsInterrupt, FALLING);
-    initProximitySensor(40);
-
     pciSetup(lmic_pins.dio[0]);
     pciSetup(lmic_pins.dio[1]);
 
@@ -263,11 +189,33 @@ void setup()
     LMIC.setArtEuiCallback(getArtEui);
 
     // set clock error to allow good connection.
-    LMIC.setClockError(MAX_CLOCK_ERROR * 3 / 100);
+    LMIC.setClockError(MAX_CLOCK_ERROR * 15 / 100);
+
+    /*
+    while(true) {
+
+        rps_t rps(
+            SF7,BandWidth::BW125, CodingRate::CR_4_5, false, 0);
+        
+        auto result = LMIC.calcAirTime(rps, 17);
+
+        PRINT_DEBUG_2 ("AirTime : %ld" , result.to_ms());
+        delay (5000);
+    }
+    */
+
+   // test duration and in case of reboot loop  prevent flood
+    
+   testDuration(1000);
+   testDuration(8000);
+   testDuration(30000);
+    
 
     // Start job (sending automatically starts OTAA too)
     do_send();
 }
+
+const int64_t sleepAdj = 1070;
 
 void powersave(OsDeltaTime const &maxTime)
 {
@@ -276,32 +224,32 @@ void powersave(OsDeltaTime const &maxTime)
     // these value are base on test
     if (maxTime > OsDeltaTime::from_ms(8700))
     {
-        duration_selected = OsDeltaTime::from_ms(8050);
+        duration_selected = OsDeltaTime::from_ms(8000 * sleepAdj /1000);
         period_selected = SLEEP_8S;
     }
     else if (maxTime > OsDeltaTime::from_ms(4600))
     {
-        duration_selected = OsDeltaTime::from_ms(4050);
+        duration_selected = OsDeltaTime::from_ms(4000 * sleepAdj /1000);
         period_selected = SLEEP_4S;
     }
     else if (maxTime > OsDeltaTime::from_ms(2600))
     {
-        duration_selected = OsDeltaTime::from_ms(2000);
+        duration_selected = OsDeltaTime::from_ms(2000 * sleepAdj /1000);
         period_selected = SLEEP_2S;
     }
     else if (maxTime > OsDeltaTime::from_ms(1500))
     {
-        duration_selected = OsDeltaTime::from_ms(1000);
+        duration_selected = OsDeltaTime::from_ms(1000 * sleepAdj /1000);
         period_selected = SLEEP_1S;
     }
     else if (maxTime > OsDeltaTime::from_ms(800))
     {
-        duration_selected = OsDeltaTime::from_ms(500);
+        duration_selected = OsDeltaTime::from_ms(500  * sleepAdj /1000);
         period_selected = SLEEP_500MS;
     }
     else if (maxTime > OsDeltaTime::from_ms(500))
     {
-        duration_selected = OsDeltaTime::from_ms(250);
+        duration_selected = OsDeltaTime::from_ms(250  * sleepAdj /1000);
         period_selected = SLEEP_250MS;
     }
     else
@@ -314,13 +262,10 @@ void powersave(OsDeltaTime const &maxTime)
     Serial.flush();
 #endif
 
-    for (uint16_t nbsleep = maxTime / duration_selected; nbsleep > 0 && !apds_new; nbsleep--)
+    for (uint16_t nbsleep = maxTime / duration_selected; nbsleep > 0; nbsleep--)
     {
         LowPower.powerDown(period_selected, ADC_OFF, BOD_OFF);
         hal_add_time_in_sleep(duration_selected);
-
-        // Check if we are wakeup by external pin.
-        apdsInterrupt();
     }
 
     PRINT_DEBUG_1("Wakeup");
@@ -343,11 +288,5 @@ void loop()
         // As an additional bonus, this prevents the can of worms that
         // we would otherwise get for running SPI transfers inside ISRs
         LMIC.io_check();
-    }
-    // was wakeup by interrupt, send new state.
-    if (apds_new)
-    {
-        apds_new = false;
-        do_send();
     }
 }
