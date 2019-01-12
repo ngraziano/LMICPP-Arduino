6218e2a28bf051120cae6595590a0df034abefce
Nicolas Graziano <nicolas.graziano@gmail.com>
2019-01-12 18:44:09 +0100

    Correct missing space in log message.

--- lib/arduino-lmic/src/hal/print_debug.h
+++ lib/arduino-lmic/src/hal/print_debug.h
@@ -10,7 +10,7 @@
 template <typename... T>
 void PRINT_DEBUG(int X, const __FlashStringHelper *str, T... div) {
   if (debugLevel >= X) {
-    printf_P(PSTR("%lu"), hal_ticks().tick());
+    printf_P(PSTR("%lu "), hal_ticks().tick());
     PGM_P p = reinterpret_cast<PGM_P>(str);
     printf_P(p, div...);
     //printf_P(PSTR("\n"));
