07e9a4abe1835dae5ba9873381323e2483b6ca1a
Nicolas Graziano <nicolas.graziano@gmail.com>
2019-10-05 11:11:04 +0200

    some formating

--- src/hal/print_debug.h
+++ src/hal/print_debug.h
@@ -1,31 +1,27 @@
 #ifndef _print_debug_h_
 #define _print_debug_
 
-
-#include "hal.h"
 #include "WString.h"
+#include "hal.h"
 #include "stdio.h"
-
 
 template <typename... T>
 void PRINT_DEBUG(int X, const __FlashStringHelper *str, T... div) {
-  if (debugLevel >= X) { 
-    #ifdef ARDUINO_ARCH_ESP32
-    // on ESP the MACRO PRIu32 cause a problem with syntax above
+  if (debugLevel >= X) {
+#ifdef ARDUINO_ARCH_ESP32
+    // on ESP the MACRO PRIu32 cause a problem with syntax below
     printf("%u ", hal_ticks().tick());
-    #else
-    printf_P(PSTR("%" PRIu32" "), hal_ticks().tick());
-    #endif
+#else
+    printf_P(PSTR("%" PRIu32 " "), hal_ticks().tick());
+#endif
     PGM_P p = reinterpret_cast<PGM_P>(str);
     printf_P(p, div...);
-    //printf_P(PSTR("\n"));
+    // printf_P(PSTR("\n"));
     printf("\n");
   }
 };
 
-constexpr bool IS_DEBUG_ENABLE(int x) {
-  return debugLevel >= x;
-}
+constexpr bool IS_DEBUG_ENABLE(int x) { return debugLevel >= x; }
 
 void hal_printf_init();
 
