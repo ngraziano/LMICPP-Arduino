d4420aced43a3f0e07ee439e7d225f0a1249389b
Nicolas Graziano <nicolas.graziano@gmail.com>
2020-10-24 18:31:36 +0200

    Simplify code.

--- examples/simple_sx1262/src/main.cpp
+++ examples/simple_sx1262/src/main.cpp
@@ -6,18 +6,14 @@
 #include <hal/hal_io.h>
 #include <hal/print_debug.h>
 #include <keyhandler.h>
+#include <algorithm>
 
 #define DEVICE_SIMPLE_SX1262
 #include "lorakeys.h"
 
-void do_send();
-void reset_and_do_send();
-void powersave(OsDeltaTime maxTime);
-
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
 constexpr OsDeltaTime TX_INTERVAL = OsDeltaTime::from_sec(135);
-
 constexpr unsigned int BAUDRATE = 9600;
 
 // Pin mapping
@@ -29,57 +25,10 @@ constexpr lmic_pinmap lmic_pins = {
 };
 // Radio class for SX1262
 RadioSx1262 radio{lmic_pins, ImageCalibrationBand::band_863_870};
+// Create an LMIC object with the right band
 LmicEu868 LMIC{radio};
-
 OsTime nextSend;
 
-void onEvent(EventType ev)
-{
-    switch (ev)
-    {
-    case EventType::JOINING:
-        PRINT_DEBUG(2, F("EV_JOINING"));
-        //        LMIC.setDrJoin(0);
-        break;
-    case EventType::JOINED:
-        PRINT_DEBUG(2, F("EV_JOINED"));
-        // disable ADR because it will be mobile.
-        LMIC.setLinkCheckMode(false);
-        break;
-    case EventType::JOIN_FAILED:
-        PRINT_DEBUG(2, F("EV_JOIN_FAILED"));
-        break;
-    case EventType::TXCOMPLETE:
-        PRINT_DEBUG(2, F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-        if (LMIC.getTxRxFlags().test(TxRxStatus::ACK))
-        {
-            PRINT_DEBUG(1, F("Received ack"));
-        }
-        if (LMIC.getDataLen())
-        {
-            PRINT_DEBUG(1, F("Received %d bytes of payload"), LMIC.getDataLen());
-            auto data = LMIC.getData();
-            if (data)
-            {
-                uint8_t port = LMIC.getPort();
-            }
-        }
-
-        break;
-    case EventType::RESET:
-        PRINT_DEBUG(2, F("EV_RESET"));
-        break;
-    case EventType::LINK_DEAD:
-        PRINT_DEBUG(2, F("EV_LINK_DEAD"));
-        break;
-    case EventType::LINK_ALIVE:
-        PRINT_DEBUG(2, F("EV_LINK_ALIVE"));
-        break;
-    default:
-        PRINT_DEBUG(2, F("Unknown event"));
-        break;
-    }
-}
 
 void do_send()
 {
@@ -98,10 +47,11 @@ void do_send()
 // PCI1 PCINT[14:8]
 // PCI0 PCINT[7:0]
 
+/* INTERUPT ON CHANGE OPTIONAL
 ISR(PCINT0_vect)
 {
     // one of pins D8 to D13 has changed
-    // store time, will be check in OSS.runloopOnce()
+    // store time, will be check in LMIC.run()
     LMIC.store_trigger();
 }
 
@@ -112,14 +62,27 @@ void pciSetup(byte pin)
     PCICR |= bit(digitalPinToPCICRbit(pin));                   // enable interrupt for the group
 }
 
+INTERUPT ON CHANGE OPTIONAL
+*/
 void setup()
 {
+    // To handle VCC <= 2.4v
+    // clock start at 8MHz / 8 => 1 MHz
+    // set clock to 8MHz / 4 => 2MHz
+    // maybe 4Mhz could also work
+    clock_prescale_set(clock_div_4);
     if (debugLevel > 0)
     {
         Serial.begin(BAUDRATE);
     }
 
-    pciSetup(lmic_pins.dio[1]);
+    if (debugLevel > 0)
+    {
+        Serial.begin(BAUDRATE);
+    }
+
+    // INTERUPT ON CHANGE OPTIONAL
+    // pciSetup(lmic_pins.dio[1]);
 
     SPI.begin();
     // LMIC init
@@ -128,33 +91,50 @@ void setup()
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC.reset();
 
-    LMIC.setEventCallBack(onEvent);
     SetupLmicKey<appEui, devEui, appKey>::setup(LMIC);
     // set clock error to allow good connection.
     LMIC.setClockError(MAX_CLOCK_ERROR * 1 / 100);
 
     // Start job (sending automatically starts OTAA too)
    nextSend = os_getTime();
+   PRINT_DEBUG(1, F("END SETUP"));
+
 }
 
 void loop()
 {
     OsDeltaTime freeTimeBeforeNextCall = LMIC.run();
-    if (freeTimeBeforeNextCall > OsDeltaTime::from_ms(10))
+    if (freeTimeBeforeNextCall < OsDeltaTime::from_ms(100))
     {
-        // we have more than 10 ms to do some work.
-        // the test must be adapted from the time spend in other task
-        if (LMIC.getOpMode().test(OpState::TXRXPEND))
-        {
-            PRINT_DEBUG(1, F("OpState::TXRXPEND, not sending"));
-        }
-        else if (nextSend < hal_ticks())
-        {
-            do_send();
-        }
-        else
-        {
-            // sleep if we have nothing to do.
-        }
+        // not enought time, do nothing else.
+        return;
+    }
+
+    if (LMIC.getOpMode().test(OpState::TXRXPEND))
+    {
+        // OpState::TXRXPEND, do not try to send new message
+        // so wait (with 100ms of marging) and return
+        auto waittime = freeTimeBeforeNextCall.to_ms()-100;
+        PRINT_DEBUG(1, F("Delay TXRXPEND %dms"), waittime);
+        delay(waittime);
+        return;
+    }
+
+    // we have more than 100 ms to do some work.
+    // the test must be adapted from the time spend in other task
+    auto timebeforesend = nextSend - hal_ticks();
+    PRINT_DEBUG(1, F("Time before send %ds"), timebeforesend.to_s());
+
+    if (timebeforesend < OsDeltaTime(0) )
+    {
+        // time to send now.
+        do_send();
+    }
+    else
+    {
+        // sleep if we have nothing to do.
+        auto sleeps = std::min(timebeforesend, freeTimeBeforeNextCall).to_s();
+        PRINT_DEBUG(1, F("Sleep %ds"), sleeps);
+        delay(sleeps * 1000);
     }
 }
